47b460ed6853b3c4d5aaa93729e841a56fc11b5b
DEVAVISWA G <devaviswa15@gmail.com>
2023-10-22 01:30:51 +0530

    day26Task-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Notes from './components/Notes'
-import Users from './components/Users'
+import All from './components/All'
+import FullStackDev from './components/FullStackDev'
+import DataScience from './components/DataScience'
+import CyberSecurity from './components/CyberSecurity'
+import Career from './components/Career'
 
-
-function App({notes}) {
-  const padd= {
-    padding: 10    
-  }
+function App() {
   return (
     <Router>
       <div>
-        <Link to= '/' style={padd}>HOME</Link>
-        <Link to= '/notes' style={padd}>NOTES</Link>
-        <Link to= '/users' style={padd}>USERS</Link>
+        <ul className="nav justify-content-center  ">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to={'/'}>All</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={'/category/full-stack-development'}>FULL STACK DEVELOPMENT</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={'/category/data-science'}>DATA SCIENCE</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={'/cyber-security'}>CYBER SECURITY</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to={'/career'}>CAREER</Link>
+          </li>
+        </ul>
       </div>
       <Routes>
-        <Route path='/' element={<Home />}>  </Route>
-        <Route path='/notes' element={<Notes notes={notes} />}>  </Route>
-        <Route path='/users' element={<Users />}>  </Route>        
+        <Route path='/' element={<All />} />
+        <Route path='/category/full-stack-development' element={<FullStackDev />} />
+        <Route path='/category/data-science' element={<DataScience />} />
+        <Route path='/cyber-security' element={<CyberSecurity />} />
+        <Route path='/career' element={<Career />} />
       </Routes>
     </Router>
   )
